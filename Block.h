75e068d4fbfa11fc35324d77f285a326fc1bf3b4
RodrigoMato00 <85580712+RodrigoMato00@users.noreply.github.com>
2022-06-20 21:04:28 -0300

    block.h

--- Block.h
+++ Block.h
@@ -4,9 +4,14 @@
 #include <cstdint>
 #include <iostream>
 
+//All the files in the C++ standard library declare all
+//of its entities within the std namespace
 using namespace std;
 
+//calling the class Block
 class Block {
+
+//public modifier and public variables
 public:
     string sPrevHash;
 
@@ -16,13 +21,17 @@ public:
 
     void MineBlock(uint32_t nDifficulty);
 
+//pivate modifier and private variables
 private:
-    uint32_t _nIndex;
-    int64_t _nNonce;
-    string _sData;
-    string _sHash;
-    time_t _tTime;
+    uint32_t _nIndex; //index of the block
+    int64_t _nNonce; //nonce of the block
+    string _sData; //data of the block
+    string _sHash; //hash of the block
+    time_t _tTime; //time of the block
 
     string _CalculateHash() const;
+    // _CalculateHash (line 18) also has the const keyword, this is
+    // to ensure the method cannot change any of the variables in the block
+    // class which is very useful when dealing with a blockchain.
 };
 #endif
